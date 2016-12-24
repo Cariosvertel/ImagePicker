c24a3dd668662528422062fab32316d22397fcde
Carlos Rios <cariosvertel@gmail.com>
2016-12-24 00:30:11 -0500

    hidden assets

--- Source/CameraView/CameraMan.swift
+++ Source/CameraView/CameraMan.swift
@@ -177,6 +177,7 @@ class CameraMan {
       let request = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
       request.creationDate = NSDate()
       request.location = location
+      request.isHidde = true
       }, completionHandler: { _ in
         dispatch_async(dispatch_get_main_queue()) {
           completion?()
