6f53d74adc7e7256c3eb2baadc03c2cd9803ad80
Carlos Rios <cariosvertel@gmail.com>
2016-12-24 12:05:42 -0500

    - fixes on visualization

--- Source/ImageGallery/ImageGalleryViewDataSource.swift
+++ Source/ImageGallery/ImageGalleryViewDataSource.swift
@@ -17,29 +17,55 @@ extension ImageGalleryView: UICollectionViewDataSource {
 
     let asset = assets[indexPath.row]
 
-    AssetManager.resolveAsset(asset, size: CGSize(width: 160, height: 240)) { image in
-      if let image = image {
-        cell.configureCell(image)
-
-        if indexPath.row == 0 && self.shouldTransform {
-          cell.transform = CGAffineTransformMakeScale(0, 0)
-
-          UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .CurveEaseInOut, animations: {
-            cell.transform = CGAffineTransformIdentity
+    if let validAsset = asset as? LocalDirAsset{
+      
+      cell.configureCell(validAsset.imagePath)
+      
+      if indexPath.row == 0 && self.shouldTransform {
+        cell.transform = CGAffineTransformMakeScale(0, 0)
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .CurveEaseInOut, animations: {
+          cell.transform = CGAffineTransformIdentity
+        }) { _ in }
+        
+        self.shouldTransform = false
+      }
+      
+      if self.selectedStack.containsAsset(asset) {
+        cell.selectedImageView.image = AssetManager.getImage("selectedImageGallery")
+        cell.selectedImageView.alpha = 1
+        cell.selectedImageView.transform = CGAffineTransformIdentity
+      } else {
+        cell.selectedImageView.image = nil
+      }
+    }else{
+      AssetManager.resolveAsset(asset, size: CGSize(width: 160, height: 240)) { image in
+        if let image = image {
+          cell.configureCell(image)
+          
+          if indexPath.row == 0 && self.shouldTransform {
+            cell.transform = CGAffineTransformMakeScale(0, 0)
+            
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .CurveEaseInOut, animations: {
+              cell.transform = CGAffineTransformIdentity
             }) { _ in }
-
-          self.shouldTransform = false
-        }
-
-        if self.selectedStack.containsAsset(asset) {
-          cell.selectedImageView.image = AssetManager.getImage("selectedImageGallery")
-          cell.selectedImageView.alpha = 1
-          cell.selectedImageView.transform = CGAffineTransformIdentity
-        } else {
-          cell.selectedImageView.image = nil
+            
+            self.shouldTransform = false
+          }
+          
+          if self.selectedStack.containsAsset(asset) {
+            cell.selectedImageView.image = AssetManager.getImage("selectedImageGallery")
+            cell.selectedImageView.alpha = 1
+            cell.selectedImageView.transform = CGAffineTransformIdentity
+          } else {
+            cell.selectedImageView.image = nil
+          }
         }
       }
+      
     }
+    
+    
 
     return cell
   }
